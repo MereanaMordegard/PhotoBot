bc2ea7d4247ed5404089a9c93f4364f60068bcb3
Dmitriy Kolemagin <dkolemagin@gmail.com>
2023-10-17 15:34:51 +0300

    добавление функции обработки фото

--- main.py
+++ main.py
@@ -8,3 +8,25 @@ from dotenv import load_dotenv
 
 load_dotenv()
 bot = os.getenv('tg_token')
+
+# создание директории для временных файлов, если ее нет.
+TEMP_PATH = 'temp/'
+os.makedirs(TEMP_PATH, exist_ok=True)
+
+# Функция для обработки фотографий и добавления водяного знака.
+def process_photo(photo_file):
+    # Загрузка фотографии.
+    photo = Image.open(photo_file)
+
+    # Добавление водяного знака.
+    draw = ImageDraw.Draw(photo)
+    font = ImageFont.load_default()
+    watermark_text = "@melisad_sosedi"
+    text_width, text_height = draw.textsize(watermark_text, font)
+    watermark_position = (photo.width - text_width, photo.height - text_height)
+    draw.text(watermark_position, watermark_text, fill=(255, 255, 255, 64), font=font)
+
+    # Сохранение обработанной фотографии.
+    processed_file = TEMP_PATH + 'processed_photo.jpg'
+    photo.save(processed_file)
+    return processed_file
